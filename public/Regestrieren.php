9fbf7abc3a62f7f106ed3554f1f20dc3280f3a69
Havenbreaker12 <E0462339@eaton.ad.etn.com>
2019-04-01 09:26:46 +0200

    Registration

--- public/Regestrieren.php
+++ public/Regestrieren.php
@@ -1,5 +1,9 @@
 <!DOCTYPE html>
 <html>
+
+<link rel="stylesheet" href="/Zocker_Knaelter/public/asset/css/style.css">
+
+  
   <head>
     <link rel="stylesheet" href="//stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="/Zocker_Knaelter/public/asset/css/style.css">
@@ -15,7 +19,7 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
-
+            
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Login</a>
@@ -41,6 +45,34 @@
     </nav>
     <!-- overview, welcome, ... -->
 
-    <?php require_once __DIR__ . '/_footer.php'; ?>
+   <div id="regestrieren_style">
+<form>
+  <form action="/action_page.php">
+  First name:<br>
+  <input type="text" name="firstname">
+  <br>
+        Last name:<br>
+  <input type="text" name="lastname">
+ </form>
+    <form action="/action_page.php">
+Password: <br>
+    <input type="password" name="pw" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters">
+
+</form>
+
+ <form action="/action_page.php">
+E-mail: <br>
+  <input type="email" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$">
+
+</form>
+  <form action="/action_page.php">
+Picture: <br>
+      <input type="url" name="website" pattern="https?://.+" title="Include http://">
+
+</form>
+     <br><br>
+  <input type="submit" value="Submit">
+  </div>
+
+  
   </body>
-</html>
